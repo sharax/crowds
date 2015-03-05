cf64c2b5b606fa7faa27cc89e27c734845dff0be
imanolarrieta <imanol@stanford.edu>
2015-03-04 22:25:40 -0800

    Randomization

--- src/example_randomization/example_randomization.R
+++ src/example_randomization/example_randomization.R
@@ -8,7 +8,7 @@ doc <- readHTMLTable(
 
 # Get  and clean the countries and population table 
 # I also taking away China's,UK's, US's and India's flags (It would not make any sense to ask this)
-countries.info<-doc[[1]][-c(1,2,3,22,),2:3]
+countries.info<-doc[[1]][-c(1,2,3,22),2:3]
 names(countries.info) <- c('Country','Population')
 # We need to convert the population column from character factor to numeric.
 countries.info$Population<-as.numeric(gsub(",","",as.character(countries.info$Population)))
@@ -16,6 +16,6 @@ countries.info$Population<-as.numeric(gsub(",","",as.character(countries.info$Po
 countries.info$Weights <- countries.info$Population/ sum(countries.info$Population)
 
 # Random Sample 
-set.seed(6)
+set.seed(1242)
 flags<- sample(countries.info$Country,20,prob = countries.info$Weights)
 flags
