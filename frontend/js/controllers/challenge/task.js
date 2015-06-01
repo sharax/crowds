2c078fe5e1012c7264470e1bef2d5daca1d9fbd6
John Wong <jwwwwong@gmail.com>
2015-06-01 12:30:16 +0800

    updated task-question to use height-based styling

--- frontend/js/controllers/challenge/task.js
+++ frontend/js/controllers/challenge/task.js
@@ -1,74 +1,31 @@
 app.controller('challengeTaskController', function($scope, $interval, $location) {
     $scope.currentSlide = 1;
 
-    $scope.answer = undefined;
+    $scope.selectedAnswer = undefined;
     $scope.selectedCL = undefined;
 
-    $scope.confidenceLevels = [0,1,2,3,4];
+    $scope.confidenceLevels = numberArray(0, 4);
+    $scope.answers = [];
 
     var timerSize = 50;
     var timerLineWidth = 6;
-    var bodyWidth = $(document).width();
-    if(bodyWidth > 1280) {
+
+    // custom size styling for large devices (large desktops, 1280px and up)
+    if($(document).height() > 960) {
         timerLineWidth = 8;
         timerSize = 80;
     }
 
-    $scope.questions = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
-    $scope.questionContent = [
-        {
-            text: "Will this business be funded by Kickstarter?",
-            type: "options",
-            imagePath: "question-media/sockless-shoes.png",
-            answers: ["Yes", "No"]
-        },
-        {
-            text: "Will this business be funded by Kickstarter?",
-            type: "options",
-            imagePath: "question-media/sockless-shoes.png",
-            answers: ["Yes", "No"],
-            previousResponses: [{text: "Yes", count: 3}, {text: "No", count: 2}]
-        },
-        {
-            text: "What is the name of this constellation?",
-            type: "options",
-            imagePath: "question-media/constellation.png",
-            answers: ["Leo", "Apus", "Lupus", "Columba", "Gemini"],
-            previousResponses: [{text: "Leo", count: 500}, {text: "Apus", count: 2}, {text: "Lupus", count: 2},
-            {text: "Columba", count: 2}, {text: "Gemini", count: 200}]
-        },
-        {
-            text: "What is this country’s landmass?",
-            type: "number",
-            imagePath: "question-media/brazil.png",
-            previousResponses: [28497, 328472, 124, 0, 23498]
-        }
-    ];
+    $scope.questions = numberArray(0, 19);
+    var questionArr = getQuestionArr();
+    setCurrQuestion(0);
 
-    $scope.currQuestion = 0;
-    $scope.question = $scope.questionContent[$scope.currQuestion];
-
-    $scope.previousResponses = [];
-    $scope.previousResponseCount = 0;
-
-    $scope.timerPercent = 0;
-    $scope.timeLeft = 30;
-    $scope.timerOptions = {
-        animate:{
-            duration:300,
-            enabled:true
-        },
-        barColor:'#feeed9',
-        scaleColor:false,
-        trackColor:'#f7a32b',
-        lineWidth:timerLineWidth,
-        lineCap:'circle',
-        size: timerSize
-    };
+    initTimer();
+    resetTimer();
     startTimer();
 
     $scope.validInput = function(answer, selectedCL) {
-        if(selectedCL === undefined || selectedCL < 0 || selectedCL > 4) {
+        if(!isValidCL(selectedCL)) {
             return false;
         }
         if(!isValidAnswer(answer)) {
@@ -81,41 +38,32 @@ app.controller('challengeTaskController', function($scope, $interval, $location)
         $scope.selectedCL = cl;
     }
 
-    $scope.setAnswer = function(answer) {
-        $scope.answer = answer;
+    $scope.selectAnswer = function(answer) {
+        $scope.selectedAnswer = answer;
     }
 
     $scope.next = function() {
         next();
     }
 
-    $scope.setQuestion = function(index) {
-        $scope.currQuestion = index;
-        $scope.question = $scope.questionContent[$scope.currQuestion];
-        $scope.previousResponses = $scope.question.previousResponses;
-        updatePreviousResponseCount();
+    $scope.setCurrQuestion = function(index) {
+        setCurrQuestion(index);
     }
 
-    function updatePreviousResponseCount() {
-        $scope.previousResponseCount = 0;
-        if($scope.question.type === "number") {
-            $scope.previousResponseCount = $scope.previousResponses.length;
-            return;
+    function isValidCL(cl) {
+        var minCL = $scope.confidenceLevels[0];
+        var maxCL = $scope.confidenceLevels[$scope.confidenceLevels.length-1];
+        if(cl === undefined || !isInt(cl) || cl < minCL || cl > maxCL) {
+            return false;
         }
-        for(var i = 0; i < $scope.previousResponses.length; i++) {
-            var response = $scope.previousResponses[i];
-            $scope.previousResponseCount += response.count;
-        }
-    }
-
-    function startTimer() {
-        $interval(function(){
-            $scope.timeLeft--;
-            $scope.timerPercent += 3.33;
-        }, 1000, 30);
+        return true;
     }
 
     function isValidAnswer(answer) {
+        if($scope.question.type === 'number') {
+            return isInt(answer);
+        }
+
         for(var i = 0; i < $scope.answers.length; i++) {
             if($scope.answers[i] === answer) {
                 return true;
@@ -125,13 +73,39 @@ app.controller('challengeTaskController', function($scope, $interval, $location)
     }
 
     function next() {
+        setCurrQuestion($scope.currQuestion + 1);
+    }
+
+    function setCurrQuestion(index) {
+        if(index === 4) {
+            $location.path("challenge/done");
+            return;
+        }
+        $scope.currQuestion = index;
+        $scope.question = questionArr[$scope.currQuestion];
+        $scope.previousResponses = $scope.question.previousResponses;
+        $scope.answers =$scope.question.answers;
+        updatePreviousResponseCount();
+
+        $scope.selectedAnswer = undefined;
+        $scope.selectedCL = undefined;
+
         switchSlides();
         resetTimer();
-        $scope.setQuestion($scope.currQuestion + 1);
-        $scope.answer = undefined;
-        $scope.selectedCL = undefined;
-        if($scope.currQuestion === 2) {
-            $location.path("challenge/done");
+    }
+
+    function updatePreviousResponseCount() {
+        $scope.previousResponseCount = 0;
+        if($scope.previousResponses === undefined) {
+            return;
+        }
+        if($scope.question.type === "number") {
+            $scope.previousResponseCount = $scope.previousResponses.length;
+            return;
+        }
+        for(var i = 0; i < $scope.previousResponses.length; i++) {
+            var response = $scope.previousResponses[i];
+            $scope.previousResponseCount += response.count;
         }
     }
 
@@ -143,8 +117,65 @@ app.controller('challengeTaskController', function($scope, $interval, $location)
         }
     }
 
+    function initTimer() {
+        $scope.timerOptions = {
+            animate:{
+                duration:300,
+                enabled:true
+            },
+            barColor:'#feeed9',
+            scaleColor:false,
+            trackColor:'#f7a32b',
+            lineWidth:timerLineWidth,
+            lineCap:'circle',
+            size: timerSize
+        };
+    }
+
+    function startTimer() {
+        $interval(function(){
+            $scope.timeLeft--;
+            $scope.timerPercent += 3.33;
+        }, 1000, 30);
+    }
+
     function resetTimer() {
         $scope.timeLeft = 30;
         $scope.timerPercent = 0;
+    }
+
+
+    function getQuestionArr() {
+        var questions = [
+            {
+                text: "Will this business be funded by Kickstarter?",
+                type: "options",
+                imagePath: "question-media/sockless-shoes.png",
+                answers: ["Yes", "No"]
+                //answers: ["Mary Poppins", "Star Wars", "Jurrasic Park", "Titanic"]
+            },
+            {
+                text: "Will this business be funded by Kickstarter?",
+                type: "options",
+                imagePath: "question-media/sockless-shoes.png",
+                answers: ["Yes", "No"],
+                previousResponses: [{text: "Yes", count: 3}, {text: "No", count: 2}]
+            },
+            {
+                text: "What is the name of this constellation?",
+                type: "options",
+                imagePath: "question-media/constellation.png",
+                answers: ["Leo", "Apus", "Lupus", "Columba", "Gemini"],
+                previousResponses: [{text: "Leo", count: 500}, {text: "Apus", count: 2}, {text: "Lupus", count: 2},
+                    {text: "Columba", count: 2}, {text: "Gemini", count: 200}]
+            },
+            {
+                text: "What is this country’s landmass in square meters?",
+                type: "number",
+                imagePath: "question-media/brazil.png",
+                previousResponses: [28497, 328472, 124, 0, 23498]
+            }
+        ];
+        return questions;
     }
 });
--- frontend/js/helper.js
+++ frontend/js/helper.js
@@ -1,3 +1,14 @@
 function isInt(value) {
     return !isNaN(parseInt(value,10)) && (parseFloat(value,10) == parseInt(value,10));
 }
+
+function numberArray(start, end, interval) {
+    if(interval === undefined) {
+        interval = 1;
+    }
+    var arr = [];
+    for(var i = start; i <= end; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
